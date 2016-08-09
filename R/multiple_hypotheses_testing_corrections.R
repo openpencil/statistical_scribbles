48c2c89aa8bb7e1bca510da04f3dafeadd0c786f
openpencil <jyoti.shankar@icloud.com>
2016-08-09 12:20:21 -0400

    some more uniformity to reflection comments + code

--- R/multiple_hypotheses_testing_corrections.R
+++ R/multiple_hypotheses_testing_corrections.R
@@ -1,5 +1,5 @@
 #' ## Multiple hypothesis testing and correcting for the resulting errors. ##
-#' ### A R-code-based walkthrough of concepts and intuition. ###
+#' ### An R-code-based walkthrough of concepts and intuition. ###
 
 #' ##### Load libraries #####
 library(ggplot2)
@@ -29,7 +29,7 @@ visualize_distribution <- function(datasub, name_of_column_to_plot, fillwhat = "
 #' #### SECTION I: What is hypothesis testing? ####
 
 #' ##### 1. Generate some data #####
-#' Seeds introduce variability ##
+#' Seeds represent sampling variability
 
 #' set.seed(149754)
 
@@ -125,11 +125,11 @@ table(pdata$nocorrection_error)
 #' How would your answer change if you had not known the truth?
 #' THINK ALOUD: Can you think of examples where you would accept this result and where you would not?
 
-#'### SECTION III: What is multiple hypothesis testing correction? ####
+#' #### SECTION III: What is multiple hypothesis testing correction? ####
 #' We will explore 3 most popular ways of correcting some of the errors
 #' and achieving control of Type I errors.
 
-#'### 1. The Bonferroni correction ####
+#' #### 1. The Bonferroni correction ####
 #' Described by Olive Jean Dunn in 1959 and 1961.
 #' Bonferroni formulated the Bonferroni inequality which helped Dunn formulate the correction.
 #' We want only 0.05 errors per test. (i.e. <1 errors per test).
@@ -138,7 +138,7 @@ table(pdata$nocorrection_error)
 #' Divide the 0.05 significance level by the number of tests.
 #' Controls the "Family-wise" error rate.
 
-pdata$bonferroni_h0reject <- ifelse(pdata$pvalues <= 0.05/nrow(pdata), "rejecth0", "accepth0")
+pdata$bonferroni_h0reject <- ifelse(pdata$pvalues <= 0.05/length(pdata$pvalues), "rejecth0", "accepth0")
 pdata$bonferroni_error <- ifelse(pdata$annot == "nodiff" & pdata$bonferroni_h0reject == "rejecth0",
                                  "FalsePositives",
                                  ifelse(pdata$annot == "diff" & pdata$bonferroni_h0reject == "accepth0",
@@ -147,15 +147,18 @@ pdata$bonferroni_error <- ifelse(pdata$annot == "nodiff" & pdata$bonferroni_h0re
 table(pdata$bonferroni_error)
 #' Look at the error table without corrections:
 table(pdata$nocorrection_error)
-#' STOP: How do results with the bonferroni correction compare to the results with no corrections?
-#' THINK: Is this result acceptable to you?
-#' THINK ALOUD: What has happened to power - i.e. True positive rate.
+
+#' ##### Stop and reflect #####
+#' How do results with the bonferroni correction compare to the results with no corrections?
+#' Is this result acceptable to you?
+#' What has happened to power - i.e. True positive rate/Sensitivity or the proportion of positives
+#' that are correctly identified as positives.
 
 
 
-#'### 2a. The Benjamini Hochberg threshold ####
+#' #### 2a. The Benjamini Hochberg threshold ####
 #' Was presented in a paper in 1995, inspired by substantial body of works from 1979 onwards.
-#' It took more than 20 years for theory behind the threshold to be formalized and gain acceptance.
+#' Took more than 20 years for theory behind the threshold to be formalized and gain acceptance.
 #' For context see: https://en.wikipedia.org/wiki/False_discovery_rate#Literature
 
 #' Can we improve power of our tests? We can tolerate some errors in return.
@@ -164,17 +167,18 @@ table(pdata$nocorrection_error)
 #' least strict for the higher p-values, with varying degrees of strictness
 #' in between? In other words, can we apply a more nuanced Bonferroni correction?
 
-#' order p-values in increasing order (i.e. lowest p-value is at the top)
+#' Order p-values in increasing order (i.e. lowest p-value is at the top)
 pdata <- pdata[order(pdata$pvalues),]
-#' assign ranks to the p-values (i.e. lowest p-value gets the lowest rank)
-pdata$BHrank <- seq_along(pdata$pvalues)
-#' calculate the Benjamini-Hochberg threshold: rank/length(p-values) * 0.05
-#' lowest p-value will be compared to the lowest threshold, i.e. the harshest threshold
-#' highest p-value with be compared to the most lenient (highest) threshold (rank/length(p-values) * 0.050 = 1 * 0.05 = 0.05)
-pdata$BH_threshold <- (0.05/nrow(pdata))*(pdata$BHrank)
-#' test at which rank, p-values are less than or equal to rank/length(p-values) * 0.05
+#' Assign ranks to the p-values (i.e. lowest p-value gets the lowest rank)
+pdata$BHrank <- rank(pdata$pvalues, ties.method = "max")
+#' Calculate the Benjamini-Hochberg threshold: 0.05/length(p-values) * rank
+pdata$BH_threshold <- (0.05/length(pdata$pvalues))*(pdata$BHrank)
+#' Lowest p-value will be compared to the lowest threshold, i.e. the harshest threshold
+#' Highest p-value with be compared to the highest threshold i.e. the most lenient threshold
+#' Test at which rank, p-values are less than or equal to the BH threshold
 BH_turningpoint <- max(which(pdata$pvalues <= pdata$BH_threshold))
 pdata$BH_h0reject <- ifelse(pdata$BHrank <= BH_turningpoint, "rejecth0", "accepth0")
+#' Calculate the errors
 pdata$BH_error <- ifelse(pdata$annot == "nodiff" & pdata$BH_h0reject == "rejecth0",
                          "FalsePositive",
                          ifelse(pdata$annot == "diff" & pdata$BH_h0reject == "accepth0",
@@ -185,12 +189,13 @@ table(pdata$BH_error)
 table(pdata$bonferroni_error)
 table(pdata$nocorrection_error)
 
-#' STOP: How do results employing the BH threshold compare to the Bonferroni and the uncorrected 0.05 threshold?
-#' THINK: Is this result acceptable to you?
-#' THINK ALOUD: Can you think about examples where you would pick this method rather than Bonferroni or no corrections at all?
+#' ##### Stop and reflect #####
+#' How do results employing the BH threshold compare to the Bonferroni and the uncorrected 0.05 threshold?
+#' Is this result acceptable to you?
+#' Can you think about examples where you would pick this method rather than Bonferroni or no corrections at all?
 
 
-#'### 2b. Smyth's interpretation of BH threshold + StoreyTibs monotonic correction: "The BH adjusted p-value" ####
+#' #### 2b. Smyth's interpretation of BH threshold: "The BH adjusted p-value" ####
 #' Smyth (of LIMMA fame) took the BH idea and blended it with Storey and Tibshirani's method of
 #' achieving monotonicity to contribute a function called "p.adjust()" to R in 2002.
 
@@ -207,13 +212,15 @@ table(pdata$nocorrection_error)
 pdata <- pdata[order(pdata$pvalues, decreasing = T),]
 #' assign ranks to the p-values (i.e. highest p-value gets highest rank)
 pdata$Smythrank <- rank(pdata$pvalues, ties.method = "max")
-#' On these ordered p-values, we will now apply the Smythhack equation
-pdata$Smythhack <- pdata$pvalues * (nrow(pdata)/pdata$Smythrank)
+#' On these ordered p-values, apply the Smythhack equation: p-value * n/rank
+pdata$Smythhack <- pdata$pvalues * (length(pdata$pvalues)/pdata$Smythrank)
 #' LOOK at the values. Specifically, look at the order of the values
 head(pdata$Smythhack, 50)
 head(rank(pdata$Smythhack, ties.method = "max"), 50)
 #' Compare these to the rank of the original p-values:
 head(pdata$Smythrank, 50)
+
+#' ##### Stop and reflect #####
 #' What is wrong here?
 #' Even through the original p-values were ordered, the SmythHack results are not ordered (i.e. not monotonic)
 #' Why is this bad?
@@ -243,7 +250,7 @@ for (rank in pdata$Smythrank) {
 padjust_check <- p.adjust(p = pdata$pvalues, method = "BH")
 pdata[which(abs(pdata$Smythhack_monotonic - pdata$padjust_check) > 0.01), ]
 
-#' Compute the FP, FN and correct calls.
+#' Compute the errors
 pdata$smythhack_h0reject <- ifelse(pdata$Smythhack_monotonic <= 0.05, "rejecth0", "accepth0")
 pdata$smythhack_error <- ifelse(pdata$annot == "nodiff" & pdata$smythhack_h0reject == "rejecth0",
                                 "FalsePositive",
@@ -255,29 +262,33 @@ table(pdata$smythhack_error)
 table(pdata$BH_error)
 table(pdata$bonferroni_error)
 table(pdata$nocorrection_error)
-#' OBSERVE: Is there a difference between the results from the SmythHack and from applying the BH threshold?
-#' THINK ALOUD: Why use the Smythhack adjustment instead of the original BH threshold?
+
+#' ##### Stop and reflect #####
+#' Is there a difference between the results from the SmythHack and from applying the BH threshold?
+#' Why use the Smythhack adjustment instead of the original BH threshold?
 
 
-#'### 3. Storey's Q-value ####
+#' #### 3. Storey's Q-value ####
 #' In 2001, Storey and Tibshirani (Storey's gradschool mentor) took a more empirical viewpoint
-#' and extract some more information from the data. Instead of explicitly minimizing the
-#' errors and maximizing the power, they determined the false discovery rate, at each p-value,
-#' if that p-value were the significance level of the test. The minimum FDR at each p-value
-#' is called its q-value.
+#' of multiple hypothesis testing corrections. Their correction extracted some more information
+#' from the data. Instead of explicitly minimizing the errors and maximizing the power, they
+#' determined the false discovery rate (FDR), at each p-value, if that p-value were the significance
+#' level of the test. The minimum FDR at each p-value is called its q-value.
 
-#' Before we explore the q-value, we need to think about some concepts.
+#' Before we explore the q-value, we need to think about some concepts that Storey and Tibshirani pondered.
 #'
-#' CONCEPT I: What does the distribution of original p-values look like?
+#' ##### CONCEPT I: What does the distribution of original p-values look like? #####
 #' Visualize the distribution of values
 visualize_distribution(datasub = pdata, name_of_column_to_plot = "pvalues")
-#' DISCUSS: What kind of distribution is this? Which are null p-values? Which are the significant ones?
+#' ##### Stop and discuss #####
+#' Which are null p-values? Which are the significant ones?
+#' What kind of distribution do the null p-values have?
 #' Try increasing or decreasing the sample sizes in the mclapply loop and re-run the visualization above.
 #' Does the shape of the distribution change?
 #' We arrive at an empirical truth: Truly null p-values are _______ly distributed.
-#' So if I ask you how many null p-values occur below say, 0.75, how will you determine this?
+#' If you wanted to determine how many null p-values occur below say, 0.75, how will you determine this?
 
-#' CONCEPT II: What is the False Discovery Rate?
+#' ##### CONCEPT II: What is the False Discovery Rate? #####
 #' False discovery rate (FDR) is the proportion of discoveries that are false.
 #' i.e. FDR = (number of false discoveries) / (total number of discoveries)
 #' For any threshold (such as 0.05):
@@ -287,24 +298,25 @@ visualize_distribution(datasub = pdata, name_of_column_to_plot = "pvalues")
 #' the number of null p-values at a threshold is Total number of null p- values * threshold.
 #' i.e. FDR = P0 * length(pdata$pvalues) * threshold / length(which(pdata$pvalues < threshold))
 
-#' CONCEPT III. What is a q-value?
-#' The Q-value of any given p-value is the FDR if the given p-value were used as the threshold for calling significance.
-#' For example: The Q-value of a p-value = 0.6 is the FDR if we called all p-values below 0.6 as significant.
+#' ##### CONCEPT III. What is a q-value?
+#' The q-value of any given p-value is the FDR if the given p-value were used as the threshold for calling significance.
+#' For example: The q-value of a p-value = 0.6 is the FDR if we called all p-values below 0.6 as significant.
 
 #' So, to compute the Q-value for every p-value (or the FDR at every p-value), we need 4 quantities:
 #' 1) P0: The proportion of null p-values in the data.
 #' 2) length(pdata$pvalues) or the number of tests
 #' 3) threshold: the p-value itself
-#' 4) length(which(pdata$pvalues < threshold)): number of significant finding at that threshold
-#' and plug into the formula:
-#' Qvalue = FDR_at_a_given_pvalue = P0 * length(pdata$pvalues) * given_pvalue / length(which(pdata$pvalues < given_pvalue))
+#' 4) length(which(pdata$pvalues < threshold)): number of significant findings at that threshold
+#' and plug these into the formula:
+#' q-value = FDR_at_a_given_pvalue = P0 * length(pdata$pvalues) * given_pvalue / length(which(pdata$pvalues < given_pvalue))
 
+#' #### STEP 1: Estimate P0, the proportion of truly null p-values ####
 #' So how do we estimate P0: of the proportion of null p-values in the data?
-#'###  STEP 1: Estimate P0, the proportion of truly null p-values ####
+#' We need a good estimator; one that would estimate P0 in a stable and accurate manner.
 #' Let's first think about it and then compute it.
-#' recall the distribution of p-values
+#' Recall the distribution of p-values
 p <- visualize_distribution(datasub = pdata, name_of_column_to_plot = "pvalues")
-#' let's draw a bunch of thresholds on the distribution
+#' Let's draw a bunch of thresholds on the distribution
 sequence_of_thresholds <- seq(from = 0, to = 0.95, by = 0.05)
 p <- p + geom_vline(xintercept = sequence_of_thresholds, linetype = "dashed",colour = "grey30")
 p
@@ -323,35 +335,43 @@ proportion_true_nulls_at_each_threshold <- sapply(sequence_of_thresholds, functi
 #' Let's plot the distribution of true nulls
 qplot(x = sequence_of_thresholds, y = proportion_true_nulls_at_each_threshold)
 #' What does it look like? Non-smooth. Non-linear. Non-monotonic
-#' So the question is : At what threshold do you get the least variability or the most stability in the true
-#' proportion of nulls? In other words, what threshold will lead to a good estimator of the true proportion
-#' of nulls? To determine this, we build a spline object or in other words, fit a curve through these points.
+#' So the question Storey and Tibs wanted to answer was: At what threshold do you get the least variability
+#' or the most stability in the estimate of the true proportion of nulls? In other words, what threshold will
+#' lead to a good estimate of the true proportion of nulls? To determine this, they built a spline object
+#' or in other words, fit a curve through these points.
 cubic_spline_object <- smooth.spline(x = sequence_of_thresholds,
                                      y = proportion_true_nulls_at_each_threshold,
                                      df = 3)
+#' Computed the predictions of this spline object / curve.
 cubic_spline_predicted_pvalue <- predict(cubic_spline_object, x = sequence_of_thresholds)$y
 #' Look at the p-value predicted by the cubic spline, i.e. the smoothed p-value
 qplot(x = sequence_of_thresholds, y = cubic_spline_predicted_pvalue)
-# As the threshold approaches 1, the proportion of true nulls becomes more stable so pick the minimum prediction
+# As the threshold approaches 1, the proportion of true nulls becomes more stable.
+# So they picked the minimum predicted value of proportion of true nulls - closest to the threshold of 1
 # Since the proportion cannot exceed 1, we take a minimum of the prediction of the largest threshold and 1.
 estimate_of_true_proportion_of_nulls <- min(cubic_spline_predicted_pvalue[length(sequence_of_thresholds)], 1)
 
-#'#### STEP 2: Compute the Q-value or the FDR at every p-value ####
-#' order p-values in decreasing order (i.e. highest p-value is at the top)
+#' #### STEP 2: Compute the Q-value or the FDR at every p-value ####
+#' Order p-values in decreasing order (i.e. highest p-value is at the top)
 pdata <- pdata[order(pdata$pvalues, decreasing = T),]
-#' FDR = P0 * length(pdata$pvalues) * threshold / length(which(pdata$pvalues < threshold))
+#' Assign a rank to the p-values
 pdata$Storeyrank <- rank(pdata$pvalues, ties.method = "max")
 #' THINK: pdata$Storeyrank of a given p-value is basically the number of pvalues less than the given pvalue
+#' In other words: pdata$Storeyrank for a given_p_value = length(which(pdata$pvalues < given_pvalue))
+#' Calculate the FDR (or qvalue) for each p-value
+#' FDR = P0 * length(pdata$pvalues) * threshold / length(which(pdata$pvalues < threshold))
 pdata$qvalues <- (estimate_of_true_proportion_of_nulls * length(pdata$pvalues) * pdata$pvalues)/ pdata$Storeyrank
 
-#' Look at the order of the computed qvalues
+#' LOOK at the values. Specifically, look at the order of the values
 head(pdata$qvalues, 50)
-head(order(pdata$qvalues), 50)
-#' Not monotonic, even though we ordered the p-values before computing the qvalue!
-#' So next we make them monotonic.| i.e. the method that Storey and Tibshirani used to make qvalues monotonic
-#' and the one that Smyth copied in p.adjust()
-#
-#'#### STEP 3: Make Q-values monotonic ####
+head(rank(pdata$qvalues, ties.method = "max"), 50)
+#' Compare these to the rank of the original p-values:
+head(pdata$Storeyrank, 50)
+#' Clearly the q-values are not monotonic, even though we ordered the p-values before computing the q-value!
+#' So next we make them monotonic using Storey and Tibs' monotonicity correction method. i.e. the method that
+#' that Smyth copied in p.adjust()
+
+#' #### STEP 3: Make Q-values monotonic ####
 #' Compare each qvalue to the qvalue above.
 #' Assign the qvalue to the minimum of the two.
 
@@ -376,7 +396,7 @@ for (rank in pdata$Storeyrank) {
 pdata$qvalues_from_package <- qvalue::qvalue(p = pdata$pvalues)$qvalues
 pdata[which(abs(pdata$qvalues_monotonic - pdata$qvalues_from_package) > 0.01), ]
 
-#' Compute the FP, FN and correct calls.
+#' Compute the errors
 pdata$qvalue_h0reject <- ifelse(pdata$qvalues_monotonic <= 0.05, "rejecth0", "accepth0")
 pdata$qvalue_error <- ifelse(pdata$annot == "nodiff" & pdata$qvalue_h0reject == "rejecth0",
                                 "FalsePositive",
@@ -384,10 +404,14 @@ pdata$qvalue_error <- ifelse(pdata$annot == "nodiff" & pdata$qvalue_h0reject == 
                                        "FalseNegative",
                                        "Correct"))
 table(pdata$qvalue_error)
+
 #' Compare with the earlier error tables:
 table(pdata$smythhack_error)
 table(pdata$BH_error)
 table(pdata$bonferroni_error)
 table(pdata$nocorrection_error)
-#' THINK ALOUD: When would you use qvalue rather than any other form of correction?
-#' THINK ALOUD: What can happen when you don't have enough tests and data?
+
+#' ##### Stop and reflect #####
+#' When would you use q-value rather than any other form of correction?
+#' What can happen when you don't have enough tests and data?
+

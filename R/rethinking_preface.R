59fb2b27d69a1e978644490054f1f7110237e199
openpencil <jyoti.shankar@icloud.com>
2016-11-29 12:19:06 -0500

    shuffling C++ compiler configuration code

--- R/rethinking_preface.R
+++ R/rethinking_preface.R
@@ -4,20 +4,13 @@
 install.packages(c("coda", "mvtnorm", "devtools"))
 devtools::install_github("rmcelreath/rethinking")
 
-#### Check if the C++ compiler works ####
-library(rstan)
-fx <- inline::cxxfunction( signature(x = "integer", y = "numeric" ) , '
-    return ScalarReal( INTEGER(x)[0] * REAL(y)[0] ) ;
-' )
-# should be 10
-fx( 2L, 5 )
-
 #### Configuration for RStan from stan-dev pages on github ####
 ## https://github.com/stan-dev/rstan/wiki/Installing-RStan-on-Mac-or-Linux
 dotR <- file.path(Sys.getenv("HOME"), ".R")
 if (!file.exists(dotR)) dir.create(dotR)
 M <- file.path(dotR, "Makevars")
 if (!file.exists(M)) file.create(M)
+
 # Setting the optimization level to 3 may prevent some other R packages
 # from installing from source if they are only tested with the
 # stock R configuration
@@ -33,3 +26,11 @@ cat(M)
 Sys.setenv(MAKEFLAGS = "-j4")
 # note: omit the 's' in 'https' if you cannot handle https downloads
 install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies = TRUE)
+
+#### Check if the C++ compiler works ####
+library(rstan)
+fx <- inline::cxxfunction( signature(x = "integer", y = "numeric" ) , '
+    return ScalarReal( INTEGER(x)[0] * REAL(y)[0] ) ;
+' )
+# should be 10
+fx( 2L, 5 )
